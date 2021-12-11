a3cb91180e1349d2739583a57dd26d01897ad5ed
telkomdev-rasyid <rasyid.ufa@gmail.com>
2021-12-12 00:51:30 +0700

    Update Fallback

---
+++ pages/_offline.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export default () => (
+  <>
+    <Head>
+      <title>Kodesiana Learn</title>
+    </Head>
+    <h1>Oh no, you're offline</h1>
+    <h2>Currently we can't serve you anything, so see you latter</h2>
+  </>
+)
